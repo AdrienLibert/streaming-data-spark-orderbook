dcd1eb27f712b2e9e706037d6a406fda6e46f6bb
E229866E <adrien.libert@etu.univ-nantes.fr>
2025-04-18 18:36:00 +0800

    print on the console + add postgres + postgres with spark

--- Makefile
+++ Makefile
@@ -18,16 +18,23 @@ build_deps: # TODO: change to main branch after orderbook repo is clean
 	docker build https://github.com/AdrienLibert/orderbook.git#clean-repo-for-chart-only-purpose:src/kafka_init -t local/kafka-init
 	docker build https://github.com/AdrienLibert/orderbook.git#clean-repo-for-chart-only-purpose:src/orderbook -t local/orderbook
 	docker build https://github.com/AdrienLibert/orderbook.git#clean-repo-for-chart-only-purpose:src/traderpool -t local/traderpool
+	docker build -t local/spark:3.5.5 -f src/spark/Dockerfile src
 
-start_spark:
-	helm install spark-operator spark-operator/spark-operator --namespace spark-operator --create-namespace --wait
-	kubectl apply -f k8s/spark/
+start_spark_operator:
+	helm install spark-operator spark-operator/spark-operator --namespace spark-operator --create-namespace --wait -f helm/spark_operator/values-local.yaml
 
-stop_spark:
+stop_spark_operator:
 	helm uninstall --ignore-not-found spark-operator -n spark-operator
 
-start: start_kafka start_spark
+start: start_kafka start_spark_operator
 	helm install orderbook chart/ --namespace orderbook -f helm/orderbook/values-local.yaml
+	kubectl create secret generic postgres --from-literal=password=postgres --from-literal=postgres-password=postgres --dry-run -o yaml | kubectl apply -f -
+	helm install postgres oci://registry-1.docker.io/bitnamicharts/postgresql -n analytics --create-namespace -f helm/postgres/values-local.yaml
+	kubectl apply -f k8s/spark/
 
-stop: stop_kafka stop_spark
+stop: stop_kafka stop_spark_operator
+	helm uninstall --ignore-not-found postgres -n analytics
 	helm uninstall orderbook --ignore-not-found orderbook --namespace orderbook
+	kubectl delete --ignore-not-found pvc data-postgres-postgresql-0 -n analytics
+	kubectl delete sparkapp spark-pi-python -n analytics
+	kubectl delete pod spark-pi-python-driver --ignore-not-found -n analytics
--- src/spark/kafka_spark.py
+++ src/spark/kafka_spark.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+from pyspark.sql.functions import from_json, col, current_timestamp
 
 spark = SparkSession \
     .builder \
@@ -8,10 +10,51 @@ spark = SparkSession \
 
 kafka_options = {
     "kafka.bootstrap.servers": "bitnami-kafka.orderbook.svc.cluster.local:9092",
-    "startingOffsets": "earliest",
+    "startingOffsets": "latest",
     "subscribe": "trades.topic"
 }
 
 df = spark.readStream.format("kafka").options(**kafka_options).load()
 
-print("Starting Kafka Spark reader...")
+json_schema = StructType([
+    StructField("trade_id", StringType(), True),
+    StructField("order_id", StringType(), True),
+    StructField("quantity", IntegerType(), True),
+    StructField("price", DoubleType(), True),
+    StructField("action", StringType(), True),
+    StructField("status", StringType(), True),
+    StructField("timestamp", IntegerType(), True)
+])
+
+json_df = df.selectExpr("CAST(value AS STRING) AS value")
+
+df_final = json_df.select(from_json(col("value"), json_schema).alias("trades")) \
+                  .select("trades.*") \
+                  .filter(col("quantity") != 0) \
+                  .withColumn("processing_timestamp", current_timestamp())
+
+df_final.writeStream.format("console").outputMode("append").start().awaitTermination()
+
+# Write to console
+console_query = trades_df.writeStream \
+    .format("console") \
+    .outputMode("append") \
+    .start()
+
+write_to_postgres = batch_df.write \
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql://postgres-postgresql.analytics.svc.cluster.local:5432/analytics") \
+    .option("user", "postgres") \
+    .option("password", "postgres") \
+    .option("dbtable", "trades") \
+    .option("driver", "org.postgresql.Driver") \
+    .mode("append") \
+    .save()
+
+# Write to PostgreSQL
+postgres_query = trades_df.writeStream \
+    .foreachBatch(write_to_postgres) \
+    .outputMode("append") \
+    .start()
+
+spark.streams.awaitAnyTermination()
